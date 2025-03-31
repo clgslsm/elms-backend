efc93058487d39d915107522394c35b37b0f9c25
longduong <hoanglong434gl@gmail.com>
2025-03-31 16:31:11 +0700

    update README.md

--- src/test/java/com/example/elms/service/LeaveRequestServiceTest.java
+++ src/test/java/com/example/elms/service/LeaveRequestServiceTest.java
@@ -8,6 +8,8 @@ import com.example.elms.repository.AttachmentRepository;
 import com.example.elms.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -15,7 +17,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -47,22 +48,26 @@ class LeaveRequestServiceTest {
     void setUp() {
         MockitoAnnotations.openMocks(this);
         SecurityContextHolder.setContext(securityContext);
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        when(authentication.getName()).thenReturn("testUser");
+    }
+
+    private LeaveRequest createMockLeaveRequest(Long id, String reason) {
+        return LeaveRequest.builder()
+                .id(id)
+                .idUserSend(1L)
+                .idUserReceive(2L)
+                .reason(reason)
+                .build();
     }
 
     @Test
     void testGetAllLeaveRequests() {
-        when(securityContext.getAuthentication()).thenReturn(authentication);
-        when(authentication.getName()).thenReturn("testUser");
-
-        LeaveRequest leaveRequest = LeaveRequest.builder()
-                .id(1L)
-                .idUserSend(1L)
-                .idUserReceive(2L)
-                .reason("Vacation")
-                .build();
+        LeaveRequest leaveRequest = createMockLeaveRequest(1L, "Vacation");
         when(leaveRequestRepository.findAll()).thenReturn(Collections.singletonList(leaveRequest));
 
         List<LeaveRequest> result = leaveRequestService.getAllLeaveRequest();
+
         assertNotNull(result);
         assertEquals(1, result.size());
         assertEquals("Vacation", result.get(0).getReason());
@@ -70,27 +75,17 @@ class LeaveRequestServiceTest {
 
     @Test
     void testGetLeaveRequestById_Success() {
-        when(securityContext.getAuthentication()).thenReturn(authentication);
-        when(authentication.getName()).thenReturn("testUser");
-
-        LeaveRequest leaveRequest = LeaveRequest.builder()
-                .id(1L)
-                .idUserSend(1L)
-                .idUserReceive(2L)
-                .reason("Vacation")
-                .build();
+        LeaveRequest leaveRequest = createMockLeaveRequest(1L, "Vacation");
         when(leaveRequestRepository.findById(1L)).thenReturn(Optional.of(leaveRequest));
 
         LeaveRequest result = leaveRequestService.getLeaveRequestById(1L);
+
         assertNotNull(result);
         assertEquals("Vacation", result.getReason());
     }
 
     @Test
     void testGetLeaveRequestById_NotFound() {
-        when(securityContext.getAuthentication()).thenReturn(authentication);
-        when(authentication.getName()).thenReturn("testUser");
-
         when(leaveRequestRepository.findById(1L)).thenReturn(Optional.empty());
 
         assertThrows(ResourceNotFoundException.class, () -> leaveRequestService.getLeaveRequestById(1L));
@@ -98,55 +93,31 @@ class LeaveRequestServiceTest {
 
     @Test
     void testCreateLeaveRequest() {
-        when(securityContext.getAuthentication()).thenReturn(authentication);
-        when(authentication.getName()).thenReturn("testUser");
-
-        LeaveRequest leaveRequest = LeaveRequest.builder()
-                .idUserSend(1L)
-                .idUserReceive(2L)
-                .reason("Vacation")
-                .build();
+        LeaveRequest leaveRequest = createMockLeaveRequest(null, "Vacation");
         when(leaveRequestRepository.save(any(LeaveRequest.class))).thenReturn(leaveRequest);
 
         LeaveRequest result = leaveRequestService.createLeaveRequest(leaveRequest, Collections.emptyList());
+
         assertNotNull(result);
         assertEquals("Vacation", result.getReason());
     }
 
     @Test
     void testUpdateLeaveRequest_Success() {
-        when(securityContext.getAuthentication()).thenReturn(authentication);
-        when(authentication.getName()).thenReturn("testUser");
-
-        LeaveRequest existingLeaveRequest = LeaveRequest.builder()
-                .id(1L)
-                .idUserSend(1L)
-                .idUserReceive(2L)
-                .reason("Vacation")
-                .build();
-        LeaveRequest updatedLeaveRequest = LeaveRequest.builder()
-                .idUserSend(1L)
-                .idUserReceive(2L)
-                .reason("Updated Reason")
-                .build();
+        LeaveRequest existingLeaveRequest = createMockLeaveRequest(1L, "Vacation");
+        LeaveRequest updatedLeaveRequest = createMockLeaveRequest(null, "Updated Reason");
         when(leaveRequestRepository.findById(1L)).thenReturn(Optional.of(existingLeaveRequest));
         when(leaveRequestRepository.save(any(LeaveRequest.class))).thenReturn(updatedLeaveRequest);
 
         LeaveRequest result = leaveRequestService.updateLeaveRequest(1L, updatedLeaveRequest, Collections.emptyList());
+
         assertNotNull(result);
         assertEquals("Updated Reason", result.getReason());
     }
 
     @Test
     void testUpdateLeaveRequest_NotFound() {
-        when(securityContext.getAuthentication()).thenReturn(authentication);
-        when(authentication.getName()).thenReturn("testUser");
-
-        LeaveRequest updatedLeaveRequest = LeaveRequest.builder()
-                .idUserSend(1L)
-                .idUserReceive(2L)
-                .reason("Updated Reason")
-                .build();
+        LeaveRequest updatedLeaveRequest = createMockLeaveRequest(null, "Updated Reason");
         when(leaveRequestRepository.findById(1L)).thenReturn(Optional.empty());
 
         assertThrows(ResourceNotFoundException.class, () -> leaveRequestService.updateLeaveRequest(1L, updatedLeaveRequest, Collections.emptyList()));
@@ -154,40 +125,34 @@ class LeaveRequestServiceTest {
 
     @Test
     void testDeleteLeaveRequest_Success() {
-        when(securityContext.getAuthentication()).thenReturn(authentication);
-        when(authentication.getName()).thenReturn("testUser");
-
-        LeaveRequest leaveRequest = LeaveRequest.builder().id(1L).build();
+        LeaveRequest leaveRequest = createMockLeaveRequest(1L, null);
         when(leaveRequestRepository.findById(1L)).thenReturn(Optional.of(leaveRequest));
         doNothing().when(leaveRequestRepository).deleteById(1L);
 
         leaveRequestService.deleteLeaveRequest(1L);
+
         verify(leaveRequestRepository, times(1)).deleteById(1L);
     }
 
     @Test
     void testDeleteLeaveRequest_NotFound() {
-        when(securityContext.getAuthentication()).thenReturn(authentication);
-        when(authentication.getName()).thenReturn("testUser");
-
         when(leaveRequestRepository.findById(1L)).thenReturn(Optional.empty());
 
         assertThrows(ResourceNotFoundException.class, () -> leaveRequestService.deleteLeaveRequest(1L));
     }
 
-    @Test
-    void testGetAttachmentsByLeaveRequestId() {
-        when(securityContext.getAuthentication()).thenReturn(authentication);
-        when(authentication.getName()).thenReturn("testUser");
-
+    @ParameterizedTest
+    @ValueSource(longs = {1L, 2L})
+    void testGetAttachmentsByLeaveRequestId(Long leaveRequestId) {
         Attachment attachment = Attachment.builder()
                 .id(1L)
-                .leaveRequestId(1L)
+                .leaveRequestId(leaveRequestId)
                 .url("http://example.com/file.pdf")
                 .build();
-        when(attachmentRepository.findAllByLeaveRequestId(1L)).thenReturn(Collections.singletonList(attachment));
+        when(attachmentRepository.findAllByLeaveRequestId(leaveRequestId)).thenReturn(Collections.singletonList(attachment));
 
-        List<Attachment> result = leaveRequestService.getAttachmentsByLeaveRequestId(1L);
+        List<Attachment> result = leaveRequestService.getAttachmentsByLeaveRequestId(leaveRequestId);
+
         assertNotNull(result);
         assertEquals(1, result.size());
         assertEquals("http://example.com/file.pdf", result.get(0).getUrl());
